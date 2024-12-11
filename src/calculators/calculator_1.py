6bdf0a8db13beec328c06490b989c014fa3b9d9e
nan <ipfilus@gmail.com>
2024-12-10 23:19:21 -0300

    feat: add calculator_1 second test

--- src/calculators/calculator_1.py
+++ src/calculators/calculator_1.py
@@ -11,7 +11,7 @@ class Calculator_1:
         first_step_result = self.__first_step(splited_number)
         second_step_result = self.__second_step(splited_number)
         third_step_result = self.__third_step(splited_number)
-        
+
         result = first_step_result + second_step_result + third_step_result
 
         return {
@@ -22,12 +22,10 @@ class Calculator_1:
         }
 
     def __validate_request(self, body: dict) -> float:
-        if "number" not in body:
+        if not isinstance(body.get("number"), (int, float)):
             raise Exception("Wrong format")
-        
-        input_data = body["number"]
 
-        return input_data
+        return float(body["number"])
 
     def __first_step(self, input_data: float) -> float:
         number = (input_data / 4) + 7
--- src/calculators/calculator_1_test.py
+++ src/calculators/calculator_1_test.py
@@ -1,5 +1,6 @@
 from src.calculators.calculator_1 import Calculator_1
 from flask import request
+from pytest import raises
 
 class MockRequest:
     def __init__(self, json: dict):
@@ -13,3 +14,11 @@ def test_calculator_1():
     assert all(key in result for key in expected_keys), "Result does not contain all expected keys"
     assert isinstance(result["RESULT"], float), "Result is not a float"
     assert round(result["RESULT"], 2) == 172.96, "Result does not match expected value"
+
+def test_calculator_1_with_wrong_format():
+    calculator = Calculator_1()
+    request = MockRequest({"number": "60"})
+    with raises(Exception, match="Wrong format") as exinfo:
+        calculator.calculate(request)
+    assert str(exinfo.value) == "Wrong format"
+
