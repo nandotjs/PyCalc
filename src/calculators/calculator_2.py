0c17c8050f96a734231cc58a62c10868d20be7b3
nan <ipfilus@gmail.com>
2024-12-12 21:50:31 -0300

    feat: add rest of calulator_2 logic and test

--- src/calculators/calculator_2.py
+++ src/calculators/calculator_2.py
@@ -1,22 +1,31 @@
 from typing import List
 from flask import request as FlaskRequest
+from src.drivers.numpy_handler import NumpyHandler
 
-class Calculator2:
+class Calculator_2:
     def __init__(self) -> None:
         pass
 
     def calculate(self, request: FlaskRequest) -> dict: # type: ignore
         body = request.json
         input_data = self.__validate_request(body)
+        result = self.__process_data(input_data)
 
         return {
             "RESULT": result
         }
 
     def __validate_request(self, body: dict) -> List[float]:
-        if not isinstance(body.get("number"), (int, float)):
+        numbers = body.get("numbers")
+        if not isinstance(numbers, list) or not all(isinstance(x, (int, float)) for x in numbers):
             raise Exception("Wrong format")
 
-        return float(body["number"])
+        return [float(x) for x in numbers]
 
-    
+    def __process_data(self, input_data: List[float]) -> float:
+        processed_data = [(11 * x) ** 0.95 for x in input_data]
+        numpy_handler = NumpyHandler()
+        std_dev = numpy_handler.standard_derivation(processed_data)
+        result = 1 / std_dev if std_dev != 0 else 0  # Avoid division by zero
+
+        return result 
--- src/calculators/calculator_2_test.py
+++ src/calculators/calculator_2_test.py
@@ -1,4 +1,4 @@
-from src.calculators.calculator_2 import Calculator2
+from src.calculators.calculator_2 import Calculator_2
 from pytest import raises
 
 class MockRequest:
@@ -6,9 +6,19 @@ class MockRequest:
         self.json = json
 
 def test_calculator_2():
-    calculator = Calculator2()
+    calculator = Calculator_2()
     request = MockRequest({"numbers": [1, 2, 3]})
     result = calculator.calculate(request)
+    expected_keys = ["RESULT"]
+    assert all(key in result for key in expected_keys), "Result does not contain all expected keys"
+    assert isinstance(result["RESULT"], float), "Result is not a float"
+    assert round(result["RESULT"], 4) == 0.1365, "Result does not match expected value"
 
-    
-    assert result["RESULT"] == 6
+def test_calculator_2_with_wrong_format():
+    calculator = Calculator_2()
+    request = MockRequest({"numbers": "1, 2, 3"})
+    with raises(Exception, match="Wrong format") as exinfo:
+        calculator.calculate(request)
+    assert str(exinfo.value) == "Wrong format"
+
+
